434b17ffd72ac71c4418b0fa295065326e5d8e59
roksolanaPyshyeva <roksolana.pyshyeva@gmail.com>
2018-04-10 22:48:24 +0300

    Fixed Post method

--- homework_08/controllers/handlers.js
+++ homework_08/controllers/handlers.js
@@ -1,10 +1,11 @@
 const fs = require("fs");
-let file = fs.readFileSync('./data/storage.json');
-const data = JSON.parse(file);
+
 exports.get = (req,res) =>{
+	const data = JSON.parse(fs.readFileSync('./data/storage.json'));
 	res.status(200).send(data);
 };
 exports.getById = (req,res) => {
+	const data = JSON.parse(fs.readFileSync('./data/storage.json'));
 	let  rockstar = data.find((element) =>{
 		return element.id === parseInt(req.params.id);
 	});
@@ -15,17 +16,16 @@ exports.getById = (req,res) => {
 	}
 };
 exports.post = (req,res) =>{
+	const data = JSON.parse(fs.readFileSync('./data/storage.json'));
 	if(!req.body.name || !req.body.band || !req.body.instrument){
 		res.status(400).send('The information is incorrect !');
 	}
-
-	let  star = data.find((element) =>{
-		return element.id === parseInt(req.params.id);
-	});
-	if(star){
-		res.status(409).send('{"message": "Musician already exist."}');
-		return;
-	}else{
+	 let star = data.find(function (element) {
+        return (element.name === req.body.name && element.band === req.body.band && element.instrument === req.body.instrument);
+    });
+    if (star) {
+        res.status(409).send({"message": "Musician already exist."});
+    }else{
 		let rockstar = {
 			"id" : data.length + 1,
 			"name": req.body.name,
@@ -33,11 +33,17 @@ exports.post = (req,res) =>{
 			"instrument" : req.body.instrument
 		};
 		data.push(rockstar);
+		fs.writeFile("./data/storage.json", JSON.stringify(data), (error) => {
+        	if (error) {
+        		return res.sendStatus(500);
+        	}
+    	});
 		res.status(201).send(data);
 	}
 
 };
 exports.put = (req,res) => {
+	const data = JSON.parse(fs.readFileSync('./data/storage.json'));
 	let  rockstar = data.find((element) =>{
 		return element.id === parseInt(req.params.id);
 	});
@@ -47,10 +53,16 @@ exports.put = (req,res) => {
 	rockstar.name = req.body.name;
 	rockstar.band = req.body.band;
 	rockstar.instrument = req.body.instrument;
+	fs.writeFile("./data/storage.json", JSON.stringify(data), (error) => {
+        if (error) {
+        	return res.sendStatus(500);
+        }
+    });
 	res.status(200).send(rockstar);
 };
 
 exports.delete = (req,res) => {
+	const data = JSON.parse(fs.readFileSync('./data/storage.json'));
 	const  rockstar = data.find((element) =>{
 		return element.id === parseInt(req.params.id);
 	});
@@ -59,6 +71,11 @@ exports.delete = (req,res) => {
 	}
 	const index = data.indexOf(rockstar);
 	data.splice(index,1);
+	fs.writeFile("./data/storage.json", JSON.stringify(data), (error) => {
+        if (error) {
+        	return res.sendStatus(500);
+        }
+    });
 	res.status(200).send({"message": "Musician has been successfully removed."});
 };
 
