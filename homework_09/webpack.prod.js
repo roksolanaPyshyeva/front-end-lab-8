92c182f0ebad4cb399d09832ea226c3fa7875095
roksolanaPyshyeva <roksolana.pyshyeva@gmail.com>
2018-04-15 15:39:41 +0300

    Fixed the duplication of UglifyJSPlugin

--- homework_09/webpack.prod.js
+++ homework_09/webpack.prod.js
@@ -1,13 +1,13 @@
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = merge(common, {
    	plugins: [
         new UglifyJSPlugin(),
         new OptimizeCssAssetsPlugin(),
-        new UglifyJsPlugin()
+        new BundleAnalyzerPlugin()
    	]
 });
